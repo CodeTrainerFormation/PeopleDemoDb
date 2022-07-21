745d17c6dbe7b18b6b1c111c5f848a47cd939699
Nab <17692533+Nab69@users.noreply.github.com>
2022-07-21 10:29:21 +0200

    context

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dal;
+using DomainModel;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PeopleContext context = new PeopleContext())
+            {
+                context.Database.EnsureDeleted();
+
+                context.Database.EnsureCreated();
+
+
+                Person p = context.People.FirstOrDefault();
+
+
+            } //context.Dispose();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Dal/PeopleContext.cs
+++ Dal/PeopleContext.cs
@@ -1,7 +1,36 @@
-﻿namespace Dal
-{
-    public class PeopleContext
-    {
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
 
+namespace Dal
+{
+    public class PeopleContext : DbContext
+    {
+        #region DbSet
+        public DbSet<Person> People { get; set; }
+        #endregion
+
+        #region Constructors
+        public PeopleContext()
+            : base()
+        {
+        }
+
+        public PeopleContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        #endregion
+
+        #region Configuration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=PeopleDatabase;Integrated Security=true");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        #endregion
     }
 }

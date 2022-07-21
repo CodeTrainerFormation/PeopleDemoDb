791ffe7e01afc4e480c903970a30034d39f69027
Nab <17692533+Nab69@users.noreply.github.com>
2022-07-21 11:47:59 +0200

    queries

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,12 +9,38 @@ namespace ConsoleApp
         {
             using (PeopleContext context = new PeopleContext())
             {
-                context.Database.EnsureDeleted();
+                //context.Database.EnsureDeleted();
 
-                context.Database.EnsureCreated();
+                //context.Database.EnsureCreated();
+
+                var persons = new List<Person>()
+                {
+                    new Person() { FirstName = "Ted", LastName = "Mosby", Age = 35},
+                    new Person() { FirstName = "Barney", LastName = "Stinson", Age = 34},
+                    new Person() { FirstName = "Marshall", LastName = "Eriksen", Age = 35},
+                    new Person() { FirstName = "Lily", LastName = "Aldrin", Age = 32},
+                    new Person() { FirstName = "Robin", LastName = "Scherbatsky", Age = 38},
+                };
+                context.People.AddRange(persons);
+                context.SaveChanges();
 
 
-                Person p = context.People.FirstOrDefault();
+                // TODO : Modifier le nom de "Robin" en "Stinson"
+                // TODO : Supprimer "Lily"
+                // TODO : Ré-ajouter la personne supprimée (Lily)
+                // TODO : Afficher toutes les personnes en les triant par Age,
+                //        puis par LastName, puis par FirstName
+                // TODO : Afficher la moyenne d'age de la liste
+                // TODO : Afficher la personne avec l'ID égal à 5
+                // TODO : Récuperer un dictionnaire contenant
+                //        le firstname en clé et l'age en valeur
+                // TODO : Récuperer une liste des personnes
+                //        ayant un age supérieur ou égal à 35
+
+                // GROS GROS GROS TODO : VERIFIER LES RESULTATS
+                // DANS LA DB A CHAQUE REQUETE !!!!
+
+
 
 
             } //context.Dispose();
